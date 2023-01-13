b42cfdb204af98ebb4708db70a51809f08e6448d
matheus0mar <matheusomar.dev@gmail.com>
2023-01-13 10:27:28 -0300

    Debbuging player movement
    Fix animations and code

--- LSW Programming Interview/Assets/Scripts/PlayerMovement.cs
+++ LSW Programming Interview/Assets/Scripts/PlayerMovement.cs
@@ -6,35 +6,55 @@ public class PlayerMovement : MonoBehaviour
 {
     public Animator anim;
     public float speed;
-    
+    private bool isFacing = false;
+
     // Update is called once per frame
     void Update()
     {
         Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0f);
-        
-        if (Input.GetKey("d") || Input.GetKey("right"))
+        bool facingRight = false;
+        bool facingLeft = false;
+        //bool isFacing = false;
+
+
+        if ((Input.GetKey("d") || Input.GetKey("right")) )
         {
             transform.localScale = new Vector3(-2, 2, 2);
             transform.rotation = Quaternion.Euler(0f, 0f, 0f);
+            //isFacing = false;
+            facingRight = true;
 
         }
-        if (Input.GetKey("a") || Input.GetKey("left"))
+        if ((Input.GetKey("a") || Input.GetKey("left")) )
         {
             transform.localScale = new Vector3(2, 2, 2);
             transform.rotation = Quaternion.Euler(0f, 0f, 0f);
+            isFacing = true;
+            facingLeft = true;
 
         }
-        if (Input.GetKey("w") || Input.GetKey("up"))
+        if (Input.GetKeyDown("w") || Input.GetKey("up"))
         {
             transform.rotation = Quaternion.Euler(0f, 180f, 0f);
-            //transform.localScale = new Vector3(-1 * transform.localScale.x , 2, 2);
+            if (transform.rotation.y > 0f && facingRight)
+            {
+                //Debug.Log("facingR = " + facingRight);
+                //Debug.Log("facingL = " + facingLeft);
+                //Debug.Log("isFacing = " + isFacing);
+
+                transform.localScale = new Vector3(-1 * transform.localScale.x , 2, 2);
+            }
+            if (transform.rotation.y > 0f && facingLeft)
+            {
+                transform.localScale = new Vector3(-1 * transform.localScale.x , 2, 2);
+            }
 
         }
         if (Input.GetKey("s") || Input.GetKey("down"))
         {
             transform.rotation = Quaternion.Euler(0f, 0f, 0f);
         }
-        
+
 
         anim.SetFloat("Horizontal", movement.x);
         anim.SetFloat("Vertical", movement.y);
