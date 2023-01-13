eb21f178a6409757f98ffe67792ef48e855f01a3
matheus0mar <matheusomar.dev@gmail.com>
2023-01-13 11:19:11 -0300

    created camera controller
    Camera follow code
    cleaned player movement code

--- LSW Programming Interview/Assets/Scripts/PlayerMovement.cs
+++ LSW Programming Interview/Assets/Scripts/PlayerMovement.cs
@@ -6,22 +6,19 @@ public class PlayerMovement : MonoBehaviour
 {
     public Animator anim;
     public float speed;
-    private bool isFacing = false;
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0f);
         bool facingRight = false;
         bool facingLeft = false;
-        //bool isFacing = false;
 
 
         if ((Input.GetKey("d") || Input.GetKey("right")) )
         {
             transform.localScale = new Vector3(-2, 2, 2);
             transform.rotation = Quaternion.Euler(0f, 0f, 0f);
-            //isFacing = false;
             facingRight = true;
 
         }
@@ -29,32 +26,25 @@ public class PlayerMovement : MonoBehaviour
         {
             transform.localScale = new Vector3(2, 2, 2);
             transform.rotation = Quaternion.Euler(0f, 0f, 0f);
-            isFacing = true;
             facingLeft = true;
 
         }
-        if (Input.GetKeyDown("w") || Input.GetKey("up"))
+        if (Input.GetKey("w") || Input.GetKey("up"))
         {
             transform.rotation = Quaternion.Euler(0f, 180f, 0f);
             if (transform.rotation.y > 0f && facingRight)
-            {
-                //Debug.Log("facingR = " + facingRight);
-                //Debug.Log("facingL = " + facingLeft);
-                //Debug.Log("isFacing = " + isFacing);
-
+            {   
                 transform.localScale = new Vector3(-1 * transform.localScale.x , 2, 2);
             }
             if (transform.rotation.y > 0f && facingLeft)
             {
                 transform.localScale = new Vector3(-1 * transform.localScale.x , 2, 2);
             }
-
         }
         if (Input.GetKey("s") || Input.GetKey("down"))
         {
             transform.rotation = Quaternion.Euler(0f, 0f, 0f);
         }
-
 
         anim.SetFloat("Horizontal", movement.x);
         anim.SetFloat("Vertical", movement.y);
